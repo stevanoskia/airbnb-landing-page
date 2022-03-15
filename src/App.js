e015321e44ae7e46d6b7e29cd0817425d4d8c8c2
AljoshaIV <andrejstevanoskiiv@gmail.com>
2022-03-15 20:06:21 +0100

    commiting

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+import Card from "./Components/Card";
+import data from "./data";
+
+export default function App() {
+  const cards = data.map(item => {
+      return (
+          <Card
+              key={item.id}
+              {...item}
+              
+          />
+      )
+  })        
+  
+  return (
+      <div>
+          <Navbar />
+          <Hero />
+          <section className="cards-list">
+              {cards}
+          </section>
+      </div>
+  )
+}
